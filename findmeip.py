c6e38920844506d431ca002a25c7c3e5b77bc0e2
lusaisai <lusaisai@163.com>
2015-01-15 02:22:12 +0800

    check if web is serving.

--- findmeip.py
+++ findmeip.py
@@ -16,23 +16,27 @@ class FindMeIP:
         self.dns_servers = []
         self.resolved_ips = set()
         self.ping_results = {}
+        self.ip_with_time = []
+        self.available_ips = None
+        self.web_reachable = set()
 
     def get_dns_servers(self):
         """Get the public dns server list from public-dns.tk"""
         if self.country == 'all':
-            url = 'http://public-dns.tk/nameservers.json'
+            with open('countries.txt') as f:
+                countries = [line.strip() for line in f.readlines() if line]
+                urls = ['http://public-dns.tk/nameserver/%s.json' % country for country in countries]
         else:
-            url = 'http://public-dns.tk/nameserver/%s.json' % self.country
-        print("Getting the list of dns servers from %s" % url)
-        try:
-            data = urllib.request.urlopen(url).read().decode()
-            servers = json.loads(data)
-            for server in servers:
-                if '.' in server['ip']:
-                    self.dns_servers.append(server['ip'])
-        except IOError:
-            print("Cannot get data from %s" % url)
-            exit(1)
+            urls = ['http://public-dns.tk/nameserver/%s.json' % self.country]
+
+        lock = threading.Lock()
+        threads = []
+        for url in urls:
+            t = GetDnsServer(url, lock, self.dns_servers)
+            t.start()
+            threads.append(t)
+        for t in threads:
+            t.join()
 
     def lookup_ips(self):
         lock = threading.Lock()
@@ -60,17 +64,35 @@ class FindMeIP:
         for t in threads:
             t.join()
 
-    def show_results(self):
-        loss_less = []
+    def check_web_reachable(self):
+        lock = threading.Lock()
+        threads = []
+        for ip in self.available_ips:
+            if threading.active_count() > 50:
+                time.sleep(1)
+            t = WebRequest(ip, lock, self.web_reachable)
+            t.start()
+            threads.append(t)
+
+        for t in threads:
+            t.join()
+
+    def summarize(self):
         for k, v in self.ping_results.items():
             if v['loss'] == 0:
-                loss_less.append((k, v['time']))
-        if loss_less:
-            s = sorted(loss_less, key=lambda x: x[1])
+                self.ip_with_time.append((k, v['time']))
+
+        self.ip_with_time = sorted(self.ip_with_time, key=lambda x: x[1])
+        self.available_ips = [x[0] for x in self.ip_with_time]
+
+    def show_results(self):
+        if self.ip_with_time:
             print("IPs ordered by delay time:")
-            pprint.PrettyPrinter().pprint(sorted(loss_less, key=lambda x: x[1]))
+            pprint.PrettyPrinter().pprint(self.ip_with_time)
             print("IPs concatenated:")
-            print('|'.join([x[0] for x in s]))
+            print('|'.join(self.available_ips))
+            print("IPs serve web:")
+            print('|'.join(self.web_reachable))
         else:
             print("No available servers found")
 
@@ -78,7 +100,48 @@ class FindMeIP:
         self.get_dns_servers()
         self.lookup_ips()
         self.ping()
+        self.summarize()
+        self.check_web_reachable()
         self.show_results()
+
+class WebRequest(threading.Thread):
+    def __init__(self, ip, lock, web_reachable):
+        threading.Thread.__init__(self)
+        self.ip = ip
+        self.lock = lock
+        self.web_reachable = web_reachable
+
+    def run(self):
+        try:
+            url = 'http://' + self.ip
+            print('making web request to %s' % url)
+            response = urllib.request.urlopen(url, None, 5)
+            self.lock.acquire()
+            if response.status == 200:
+                self.web_reachable.add(self.ip)
+            self.lock.release()
+        except IOError:
+            print("Cannot get data from %s" % url)
+
+class GetDnsServer(threading.Thread):
+    def __init__(self, url, lock, dns_servers):
+        threading.Thread.__init__(self)
+        self.url = url
+        self.lock = lock
+        self.dns_servers = dns_servers
+
+    def run(self):
+        try:
+            print('retrieving dns servers from %s' % self.url)
+            data = urllib.request.urlopen(self.url).read().decode()
+            servers = json.loads(data)
+            self.lock.acquire()
+            for server in servers:
+                if '.' in server['ip']:
+                    self.dns_servers.append(server['ip'])
+            self.lock.release()
+        except IOError:
+            print("Cannot get data from %s" % self.url)
 
 
 class NsLookup(threading.Thread):
@@ -96,6 +159,9 @@ class NsLookup(threading.Thread):
             ips = self.parse_nslookup_result(output.decode())
             self.lock.acquire()
             for ip in ips:
+                # google is heavily blocked in china, most of these official addresses won't work
+                if 'google' in self.name and (ip.startswith('74.') or ip.startswith('173.')):
+                    continue
                 self.store.add(ip)
             self.lock.release()
         except subprocess.CalledProcessError:
@@ -134,8 +200,9 @@ class Ping(threading.Thread):
     @staticmethod
     def parse_ping_result(result):
         loss = result.split('\n')[-3].split(', ')[2].split(' ')[0].replace('%', '')
-        time = result.split('\n')[-2].split(' = ')[-1].split('/')[1]
-        return {'loss': float(loss), 'time': float(time)}
+        trip_time = result.split('\n')[-2].split(' = ')[-1].split('/')[1]
+        return {'loss': float(loss), 'time': float(trip_time)}
+
 
 if len(sys.argv) == 2:
     FindMeIP('www.google.com', sys.argv[1]).run()
