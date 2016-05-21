793c46867868701c220b26094cd33de639bc2745
lusaisai <lusaisai@163.com>
2016-05-21 13:19:22 +0800

    Sync up dns cache and change default settings

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
 # the number of threads(dns query or ssl service check) running concurrently
-threads = 200
+threads = 100
 
 # the maximum number of dns servers to query in a certain country
-servers = 1500
+servers = 2500
