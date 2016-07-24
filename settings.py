deb0635614f41b02f00c7cadf656bcf242a36755
lusaisai <lusaisai@163.com>
2016-07-24 23:56:42 +0800

    Upload Latest DNS Cache.

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
 # the number of threads(dns query or ssl service check) running concurrently
-threads = 100
+threads = 50
 
 # the maximum number of dns servers to query in a certain country
-servers = 2500
+servers = 5000
